3b343de24a389a556d056bbd3fd6db009d50695d
ChristianJaimes <christiandja@gmail.com>
2021-06-17 19:24:45 -0500

    se añade forma alternativa de manejar errores usando aserciones

---
+++ assert_staments.py
@@ -0,0 +1,18 @@
+def divisors(num):
+    assert int(num) > 0, "Debes ingresar numeros positivos"
+    divisors =[]
+    for i in range(1,num + 1):
+         if num % i == 0:
+             divisors.append(i)
+    return divisors
+    
+
+def run():
+    num = input("Ingresa un numero: " )
+    assert num.replace("-","").isnumeric(),"Debes ingresar un valor numerico"
+    print(divisors(int(num)))
+    print("termino el programa")
+
+if __name__ == '__main__':
+    run()
+    
