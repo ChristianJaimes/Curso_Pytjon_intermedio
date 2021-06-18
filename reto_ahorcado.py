a100e4a5fb3601555b5ee42c6067c9ab335413af
ChristianJaimes <christiandja@gmail.com>
2021-06-17 21:31:37 -0500

    se añade el reto del ahorcado sin desarrollar

---
+++ reto_ahorcado.py
@@ -0,0 +1,19 @@
+def main():
+    """           Reto del Ahorcado
+    Reglas para el desarrollo:
+     * Incorportar comprehensions,
+       manejo de errores y manejo de archivos.
+     * Utilizar el archivo data.txt para leerlo
+       y obtener las palabras  
+
+             Ayudas
+     * Funcion enumerate
+     * metodo get de los diccionarios
+     * Sentencia 
+         os.system("cls") -> en windows
+         os.system("clear") -> en Unix
+    """
+    pass
+if __name__ == '__main__':
+    main()
+    
