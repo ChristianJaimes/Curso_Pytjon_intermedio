c5b4b13a927c29f7b0f4af66b6880c42e7f34d85
ChristianJaimes <christiandja@gmail.com>
2021-06-18 23:44:48 -0500

    Se añadem funcionalidades de extraer palabra aleatoria y ocultar palabra y normalizar palabra

--- reto_ahorcado.py
+++ reto_ahorcado.py
@@ -1,3 +1,5 @@
+import os
+import random
 def main():
     """           Reto del Ahorcado
     Reglas para el desarrollo:
@@ -14,6 +16,57 @@ def main():
          os.system("clear") -> en Unix
     """
     pass
+def run():
+      os.system("cls")
+      palabraOriginal = getPalabraOriginal()
+      palabraOculta = ocultarPalabra(palabraOriginal)
+      print(palabraOculta)
+      print(getPalabraclave(palabraOriginal))
+      letra = input("ingresa una letra")
+      for i in range(0,len(palabraOculta)):
+            
+            if letra == palabraOriginal[i]:
+                  palabraOculta[i] = letra
+      print(palabraOculta)
+      palabraOculta.replace()                  
+
+def ocultarPalabra(palabraOriginal):
+      """Muestra _ en lugar de las letras de la palabra
+      """
+      palabraOculta = "_ "*len(palabraOriginal)
+      return palabraOculta[:-1]
+      
+
+def getPalabraclave(palabraOriginal):
+      """Tratamiento de la palabra original de forma que quede
+      E S P A C I A D A la palabra clave y sea facil de comparar
+      """
+      palabraClave = ""
+      tam = len(palabraOriginal)
+      for i in range(0,tam):
+                  palabraClave += palabraOriginal[i]
+                  palabraClave += " "             
+      return palabraClave[:-1]
+            
+      
+
+def getPalabraOriginal():
+      
+      """ Retorna una palabra aleatoria contenida dentro del archivo data.txt
+      ARGS : null
+      RETURN: 
+          String:
+                Palabra extraida del archivo data.txt
+      """
+      all_Words = []
+      with open ("./data.txt","r",encoding="utf-8") as file:
+        for line in file:
+              all_Words.append(line.replace("\n",""))
+      palabraClave = all_Words[random.randint(0,len(all_Words))]
+      return palabraClave
+      
+                         
+
 if __name__ == '__main__':
-    main()
+    run()
     
